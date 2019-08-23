cfb5dabd39e32daa58ad94313bdf914093897940
Saucedo <manuelsaucedo442@gmail.com>
2019-08-22 19:19:21 -0600

    se subieron los 6 primero ejemplos

---
+++ tarea 1 ejercicios del libro 1/trabajo 6/multiplicacion_dos_numeros.js
@@ -0,0 +1,6 @@
+var a = 12;
+var b = 17;
+function showProduct(frist, second) {
+    console.log("The product is: " + frist * second);
+}
+showProduct(a, b);
---
+++ tarea 1 ejercicios del libro 1/trabajo 6/multiplicacion_dos_numeros.ts
@@ -0,0 +1,8 @@
+let a: number = 12;
+let b: number = 17;
+
+function showProduct(frist: number, second: number): void{
+    console.log("The product is: "+ frist* second);
+}
+
+showProduct(a, b);
