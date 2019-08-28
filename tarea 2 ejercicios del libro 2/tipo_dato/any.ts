e92137552b804081fac59f68cf59d4e0a6eaf669
Saucedo <manuelsaucedo442@gmail.com>
2019-08-27 20:44:27 -0600

    se subio el ejemplo del tipo de datos any

---
+++ tarea 2 ejercicios del libro 2/tipo_dato/any.ts
@@ -0,0 +1,10 @@
+let notsure: any = 4
+notsure = "maybe a string instead";
+notsure = false;;
+notsure.ifItExists();
+notsure.toFixed();
+let prettySure: Object = 4;
+prettySure.toFixed();
+
+let list: any[] = [1,true,"free"];
+list[1] = 100;
