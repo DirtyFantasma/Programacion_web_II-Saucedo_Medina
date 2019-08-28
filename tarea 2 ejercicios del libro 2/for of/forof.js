596adcfd4459f7549f32227216889cad5c4e66ee
Saucedo <manuelsaucedo442@gmail.com>
2019-08-28 16:37:25 -0600

    se subieron los ejemplos de for of

---
+++ tarea 2 ejercicios del libro 2/for of/forof.js
@@ -0,0 +1,12 @@
+//for of accediendo al valor de una varible dentro de un array
+var list = ["a", "b", "c"];
+for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+    var b = list_1[_i];
+    console.log(b);
+}
+//for of accediendo al valor de un variable dentro de un string
+var string1 = "is it me you're loiking for?";
+for (var _a = 0, string1_1 = string1; _a < string1_1.length; _a++) {
+    var char = string1_1[_a];
+    console.log(char);
+}
---
+++ tarea 2 ejercicios del libro 2/for of/forof.ts
@@ -0,0 +1,11 @@
+//for of accediendo al valor de una varible dentro de un array
+let list = ["a","b","c"];
+for (let b of list){
+    console.log(b);
+}
+
+//for of accediendo al valor de un variable dentro de un string
+let string1 = "is it me you're loiking for?"
+for (let char of string1){
+    console.log(char);
+}
