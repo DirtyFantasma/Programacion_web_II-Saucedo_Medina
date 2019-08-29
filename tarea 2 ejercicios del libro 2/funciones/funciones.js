40e1c8d94d82327791ecfc1eac085ba2ce252a6e
Saucedo <manuelsaucedo442@gmail.com>
2019-08-28 20:00:50 -0600

    se subieron los ejemplos sobre funciones

---
+++ tarea 2 ejercicios del libro 2/funciones/funciones.js
@@ -0,0 +1,22 @@
+var _this = this;
+//este tipo funcion hace referencia al objeto que llama a esta función
+setTimeout(function () {
+    console.log(this);
+}, 2000);
+//este tipo de funciones, lo que hace es que el this no hace referencia al padre si no al objeto que contiene la funcion
+setTimeout(function () {
+    console.log(_this);
+}, 2000);
+//como evitar el tipo any y filtar solo por los tipos de datos que necesitamos
+function padLeft(value, padding) {
+    if (typeof padding === "number") {
+        return Array(padding + 1).join(" ") + value;
+    }
+    if (typeof padding === "string") {
+        return Array(padding.length + 1).join(" ") + value;
+    }
+    throw new Error("Expected String or number, got '" + padding + "' ");
+}
+console.log(padLeft("hello", "aaa"));
+console.log(padLeft("hello", 5));
+//console.log(padLeft("hello", true)); esta linea da error ya que hay un tipo de dato no perimitido en la funcion 
