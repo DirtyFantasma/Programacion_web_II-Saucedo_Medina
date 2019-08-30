10c2f48273666b4634608cd6a4d00ed739ad2c82
Saucedo <manuelsaucedo442@gmail.com>
2019-08-29 22:47:34 -0600

    se subieron los ejemplos de clases

---
+++ tarea 2 ejercicios del libro 2/clases/clase5.ts
@@ -0,0 +1,10 @@
+class Something {
+    static instances = 0;
+    constructor() {
+        // Acedemos directamente mediante el nombre de la clase
+        Something.instances++;
+    }
+}
+var s1 = new Something();
+var s2 = new Something();
+console.log(Something.instances);
