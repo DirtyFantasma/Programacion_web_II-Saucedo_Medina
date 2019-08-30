0ca1d61fc12d35c6d0959da7b805fda671509110
Saucedo <manuelsaucedo442@gmail.com>
2019-08-29 21:53:04 -0600

    se subieron los ejemplos de generadores

---
+++ tarea 2 ejercicios del libro 2/generadores/generadores4.ts
@@ -0,0 +1,46 @@
+function* generator() {
+    try {
+        yield 'foo';
+        throw Error("Test");
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+}
+var iterator = generator();
+var foo = iterator.next();
+console.log(foo.value);
+
+//////////
+function* generator1() {
+    try {
+        yield 'foo';
+        throw Error("Test");
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+}
+var iterator = generator();
+var foo = iterator.next();
+console.log(foo.value);
+var foo = iterator.next();
+//////////
+
+function getFirstName() {
+    setTimeout(function () {
+        gen.next('alex')
+    }, 1000);
+}
+function getSecondName() {
+    setTimeout(function () {
+        gen.next('perry')
+    }, 1000);
+}
+function* sayHello() {
+    var a = yield getFirstName();
+    var b = yield getSecondName();
+    console.log(a, b); //alex perry
+}
+var gen = sayHello();
+gen.next();
