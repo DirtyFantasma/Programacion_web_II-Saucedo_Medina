93631fb668194bfbfdbe1e565f4021f46ccc7dd7
Saucedo <manuelsaucedo442@gmail.com>
2019-08-30 22:03:36 -0600

    se subieron los ejemplos de decorators

---
+++ tarea 2 ejercicios del libro 2/Decorators/static_method_decorator/method.ts
@@ -0,0 +1,12 @@
+function StaticMethodDecorator(
+    target: Function, 
+    propertyKey: string | symbol, 
+    descriptor: TypedPropertyDescriptor<any>
+) {
+    console.log("StaticMethodDecorator called on: ", target, propertyKey, descriptor);
+}
+class StaticMethodDecoratorExample {
+    @StaticMethodDecorator
+    static staticMethod() {
+    }
+}
